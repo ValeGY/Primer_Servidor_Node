21ee033f1cc8bf3a36a9dc88acd66b439250d0a3
ValeGY <valeria.guerrero.y@gmail.com>
2025-05-21 13:25:33 -0400

    Primera carga git

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import debug from "debug";
+import env from "env-var";
+import {config} from "dotenv";
+
+//Mensaje claro consola
+const mensaje = debug("app: carpeta app.js: ");
+
+//complementa a env
+config();
+const app = express();
+const port = env.get("PuertoReferencia").required().asIntPositive();
+
+mensaje("Este es el puerto de mi de entorno virtual "+port);
+
+app.get('/', (req, res) => {
+   // res.status(200).json({message:"Hola primer servidor"})
+   res.status(200).send("<h1> prueba 1 </h1><br><h2> prueba 1 </h2>")
+
+});
+
+app.get('/Valeria', (req, res) => {
+   // res.status(200).json({message:"Hola primer servidor"})
+   res.status(200).send("Segunda URL")
+
+});
+
+app.listen(port, () => {
+    mensaje("Servidor iniciado con éxito en http://localhost:" + port);
+});
+
